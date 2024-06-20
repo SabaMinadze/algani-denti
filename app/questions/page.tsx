3f28efb7c85254ada8bb30ed6c9e20525796f963
SabaMinadze <sabatvaazz100@gmail.com>
2024-06-20 22:35:19 +0400

    Make question's route inputs functional.
    As you know in the last part of update we add new question route which have many bugs and errors and are not fully functional. in thar change we make more question route inputs functionable.

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -1,26 +1,13 @@
+'use client'
+
 import React from 'react'
-import styles from './page.module.css'
+import Contact from '../components/Contact/Contact'
 
 function page() {
   return (
     <>
-    <center><div className={styles.box}>
-      <form action="https://www.youtube.com/watch?v=zHug4Lx0HrI"> 
-        <label htmlFor="" className={styles.txt}>სახელი: </label>
-        <br/>
-        <input type="text" name="" id="" placeholder='Name' required className={styles.name}/>
-        <br/><br/>
-        <label htmlFor="" className={styles.txt}>ელ-ფოსტა: </label>
-        <br/>
-        <input type="email" name="" id="" placeholder="E-mail" required className={styles.email}/>
-        <br/><br/>
-        <label htmlFor="" className={styles.txt}>წერილი: </label>
-        <br/>
-        <input type="text" name="" id="" placeholder="Enter Your Message" required className={styles.info}/>
-        <br/><br/>
-        <input type="submit" value="მოგვწერეთ" className={styles.submit}/>
-      </form>
-    </div></center>
+    <Contact/>
+    
     </>
   )
 }
