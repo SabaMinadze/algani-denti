7f12f58a55fd34b9f8fec9b348b40567f7ef4f94
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-06-21 23:40:16 +0400

    Fix the map component's type error, while I turn into build mode.
    Type error was fixed.

--- app/components/Map/Map.tsx
+++ app/components/Map/Map.tsx
@@ -15,14 +15,19 @@ const containerStyle = {
   };
 
 function Map() {
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-      })
+
+  if (!process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY) {
+    throw new Error('NEXT_PUBLIC_GOOGLE_MAPS_API_KEY is not defined');
+}
+
+const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+})
     
       const [map, setMap] = React.useState(null)
     
-      const onLoad = React.useCallback(function callback(map) {
+      const onLoad = React.useCallback(function callback(map:any) {
         // This is just an example of getting and using the map instance!!! don't just blindly copy!
         const bounds = new window.google.maps.LatLngBounds(center);
         map.fitBounds(bounds);
@@ -30,7 +35,7 @@ function Map() {
         setMap(map)
       }, [])
     
-      const onUnmount = React.useCallback(function callback(map) {
+      const onUnmount = React.useCallback(function callback(map:any) {
         setMap(null)
       }, [])
     
