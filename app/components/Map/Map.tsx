0c770e5192b8d9a688bf257a236b06cbc34a4614
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-06-22 00:13:49 +0400

    Fix another npm run build error attempt 2nd.

--- app/components/Map/Map.tsx
+++ app/components/Map/Map.tsx
@@ -1,9 +1,20 @@
-"use client"
+"use client";
 
 import React from 'react';
-import { useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: "800px",
+    height: '400px'
+};
+
+const center = {
+    lat: 41.72324171943729,
+    lng: 44.773137968025225
+};
 
 const Map: React.FC = () => {
+
     if (!process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY) {
         throw new Error('NEXT_PUBLIC_GOOGLE_MAPS_API_KEY is not defined');
     }
@@ -15,18 +26,30 @@ const Map: React.FC = () => {
 
     const [map, setMap] = React.useState<google.maps.Map | null>(null);
 
-    // Your component logic here
+    const onLoad = React.useCallback((map: google.maps.Map) => {
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+        setMap(map);
+    }, []);
 
-    return (
-        <div>
-            {isLoaded ? (
-                // Your map component
-                <div id="map"></div>
-            ) : (
-                <div>Loading...</div>
-            )}
-        </div>
+    const onUnmount = React.useCallback((map: google.maps.Map) => {
+        setMap(null);
+    }, []);
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+        >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <></>
+        </GoogleMap>
+    ) : (
+        <div>Loading...</div>
     );
 }
 
-export default Map
+export default Map;
