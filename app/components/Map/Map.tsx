99d9a7126c844a90843e94fdbdb681bd8eaafdd7
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-06-21 23:52:21 +0400

    Fix another npm run build error

--- app/components/Map/Map.tsx
+++ app/components/Map/Map.tsx
@@ -1,57 +1,32 @@
 "use client"
 
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React from 'react';
+import { useJsApiLoader } from '@react-google-maps/api';
 
-const containerStyle = {
-    width: "800px",
-    height: '400px'
-  };
-  
-  
-  const center = {
-    lat: 41.72324171943729,
-    lng: 44.773137968025225
-  };
+const Map: React.FC = () => {
+    if (!process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY) {
+        throw new Error('NEXT_PUBLIC_GOOGLE_MAPS_API_KEY is not defined');
+    }
 
-function Map() {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    });
 
-  if (!process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY) {
-    throw new Error('NEXT_PUBLIC_GOOGLE_MAPS_API_KEY is not defined');
+    const [map, setMap] = React.useState<google.maps.Map | null>(null);
+
+    // Your component logic here
+
+    return (
+        <div>
+            {isLoaded ? (
+                // Your map component
+                <div id="map"></div>
+            ) : (
+                <div>Loading...</div>
+            )}
+        </div>
+    );
 }
-
-const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-})
-    
-      const [map, setMap] = React.useState(null)
-    
-      const onLoad = React.useCallback(function callback(map:any) {
-        // This is just an example of getting and using the map instance!!! don't just blindly copy!
-        const bounds = new window.google.maps.LatLngBounds(center);
-        map.fitBounds(bounds);
-    
-        setMap(map)
-      }, [])
-    
-      const onUnmount = React.useCallback(function callback(map:any) {
-        setMap(null)
-      }, [])
-    
-      return isLoaded ? (
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
-        >
-          { /* Child components, such as markers, info windows, etc. */ }
-          <></>
-        </GoogleMap>
-    ) : <></>
-  }
- 
 
 export default Map
