1c95eeaa76ee648f771eae744b6b04246b69b01c
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-06-22 12:00:18 +0400

    Add a new Burgernav component.
    As you and all of developers knows, one of the best thing in native design is navbar. One of the popular way is to create burger type navbar and write all of the navbar content, with that way user can navigate easier in any mobile devices, so we create new navbar component where user experience in mobile devices ease. Hope you enjoy with this update :)

---
+++ app/components/Burgernav/Burgernav.tsx
@@ -0,0 +1,26 @@
+// components/HamburgerMenu.js
+import Link from 'next/link';
+import styles from './Burgernav.module.css'
+
+const Burgernav = () => (
+  <>
+    <label className={styles.hamburgerMenu}>
+            <input type="checkbox" />
+          </label>
+          <aside className={styles.sidebar}>
+            <nav>
+            <Link href="/aboutus" className={styles.txt}><div>ჩვენს შესახებ</div></Link>
+            <br/>
+            <Link href="/stomatologi" className={styles.txt}><div>ჩვენი სტომატოლოგები</div></Link>
+            <br/>
+            <Link href="/achievements" className={styles.txt}><div>ჩვენი მიღწევები</div></Link>
+            <br/>
+            <Link href="/questions" className={styles.txt}><div>კითხვები</div></Link>
+            <br/><br/><br/><br/>
+            <Link href="/contactus" className={styles.txt2}><div>კონტაქტი</div></Link>
+            </nav>
+          </aside>
+  </>
+);
+
+export default Burgernav;
