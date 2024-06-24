23cde32f23234de809b6f6ceda4ae30b39b6078e
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-06-24 20:31:29 +0400

    English version of the website. pt3

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -7,6 +7,9 @@ function Footer() {
     <div className={styles.footer}>
       <h1 className={styles.txt}>MADE BY SABA MINADZE</h1>
       <h2 className={styles.txt3}>Instagram: minadzesaba16</h2>
+      <br/><br/>
+      <h1>Language</h1>
+      <button className={styles.btn}>English</button>
 
       <h2 className={styles.txt2}>algandent.ge ყველა უფლება დაცულია. © 2023 - 2024</h2>
 
