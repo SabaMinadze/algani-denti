fe8279ef14819922230d7813f21a144c7d755c3e
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-07-04 02:42:47 +0400

    As i think i FIX UNFIXABLE AND HARDEST BUG THAT DUCK CAN NOT FIX IT. HAHAHAHAHAHAHAHAHAHAHA

--- styles/next.config.js
+++ styles/next.config.js
@@ -7,6 +7,22 @@ const nextConfig = {
     ) {
       return {
         '/': { page: '/' },
+        '/aboutus': { page: '/aboutus' },
+        '/stomatologi': { page: '/stomatologi' },
+        '/achievements': { page: '/achievements' },
+        '/questions': { page: '/questions' },
+        '/servicesandprices': { page: '/servicesandprices' },
+        '/servicesandprices/prices': { page: '/servicesandprices/prices' },
+        '/servicesandprices/prices2': { page: '/servicesandprices/prices' },
+        '/servicesandprices/prices3': { page: '/servicesandprices/prices' },
+        '/servicesandprices/prices4': { page: '/servicesandprices/prices' },
+        '/servicesandprices/prices5': { page: '/servicesandprices/prices' },
+        '/servicesandprices/prices6': { page: '/servicesandprices/prices' },
+        '/servicesandprices/prices7': { page: '/servicesandprices/prices' },
+        '/servicesandprices/prices8': { page: '/servicesandprices/prices' },
+        '/servicesandprices/prices9': { page: '/servicesandprices/prices' },
+        '/servicesandprices/contactus': { page: '/servicesandprices/contactus' },
+
         // Add other pages as needed
       };
     },
