d572afdc83c12d4b0e1bc0a9f26704b05e81f245
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-07-04 01:16:10 +0400

    .

--- styles/next.config.js
+++ styles/next.config.js
@@ -1,7 +1,15 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        '/': { page: '/' },
+        // Add other pages as needed
+      };
+    },
   };
   
   module.exports = nextConfig;
